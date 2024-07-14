fd5723d50a46866c80c450c61aaa61b2c5d7f6f1
Konst9 <konst.mih.ter@gmail.com>
2024-07-14 17:24:54 +0300

    new commit

--- src/components/guests.jsx
+++ src/components/guests.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import dima from '../img/mar/dima.jpg';
+import geka from '../img/mar/geka.jpg';
+import kris from '../img/mar/kris.jpg';
+import polina from '../img/mar/polina.jpg';
+import vika from '../img/mar/vika.jpg';
 
 const guests = [
   { name: 'Константин Наталия', src: '/static/images/avatar/1.jpg' },
-  { name: 'Никита Кристина', src: '/static/images/avatar/1.jpg' },
+  { name: 'Никита Кристина', src: kris },
   { name: 'Борис Татьяна', src: '/static/images/avatar/1.jpg' },
-  { name: 'Виктория Денис', src: '/static/images/avatar/1.jpg' },
-  { name: 'Евгений', src: '/static/images/avatar/1.jpg' },
+  { name: 'Виктория Денис', src: vika },
+  { name: 'Евгений', src: geka },
   { name: 'Виктор Мария', src: '/static/images/avatar/1.jpg' },
   { name: 'Яков', src: '/static/images/avatar/1.jpg' },
-  { name: 'Дмитрий', src: '/static/images/avatar/1.jpg' },
-  { name: 'Полина', src: '/static/images/avatar/1.jpg' },
+  { name: 'Дмитрий', src: dima },
+  { name: 'Полина', src: polina },
   { name: 'Александр Дарья', src: '/static/images/avatar/1.jpg' },
 ];
 
